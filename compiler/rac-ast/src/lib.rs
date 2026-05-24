e41558a3deeb21c4ab0f02d1e89ee68582011f98
Roman Maksimovich <r.a.maksimovich@gmail.com>
2026-05-24 11:12:26 +0800

    ast: compute ranges of expressions

--- compiler/rac-ast/src/lib.rs
+++ compiler/rac-ast/src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::{HashMap, VecDeque};
-use rac_diagnostics::Span;
+use rac_diagnostics::{Span, join};
 
 // Nominal AST structure
 
@@ -37,16 +37,16 @@ pub struct SymbolicProgram {
 // can sometimes be computed from its subexpressions.
 
 pub enum Expr<N> {
-    // Variables
+    // Variables. The `Span` is the range of the variable
     Variable(N, Span),
 
-    // Literals
+    // Literals. The `Span` is the range of the literal
     IntLiteral(i32, Span),
     BoolLiteral(bool, Span),
     StringLiteral(String, Span),
     UnitLiteral(Span),
     
-    // Binary operators
+    // Binary operators. Range is computed as the `join` of the ranges of `lhs` and `rhs`
     Plus(Box<Expr<N>>, Box<Expr<N>>),
     Minus(Box<Expr<N>>, Box<Expr<N>>),
     Times(Box<Expr<N>>, Box<Expr<N>>),
@@ -59,26 +59,57 @@ pub enum Expr<N> {
     Equals(Box<Expr<N>>, Box<Expr<N>>),
     Concat(Box<Expr<N>>, Box<Expr<N>>),
 
-    // Unary operators
+    // Unary operators. The `Span` contains the range of *the operator*, not the whole expression
     Not(Box<Expr<N>>, Span),
     Neg(Box<Expr<N>>, Span),
 
-    // Function/constructor call
+    // Function/constructor call. The `Span` contains the range of the *entire expression*
     Call(N, VecDeque<Expr<N>>, Span),
 
     // Control flow
-    Sequence(Box<Expr<N>>, Box<Expr<N>>),
-    Let(N, Type<N>, Box<Expr<N>>, Box<Expr<N>>, Span),
-    Ite(Box<Expr<N>>, Box<Expr<N>>, Box<Expr<N>>, Span),
+    Sequence(Box<Expr<N>>, Box<Expr<N>>), // range is computed as the `join` of the ranges of `lhs` and `rhs`
+    Let(N, Type<N>, Box<Expr<N>>, Box<Expr<N>>, Span), // The `Span` contains the range of the *entire expression*
+    Ite(Box<Expr<N>>, Box<Expr<N>>, Box<Expr<N>>, Span), // The `Span` contains the range of the *entire expression*
 
-    // Pattern matching
+    // Pattern matching. The `Span` contains the range of the *closing curly bracket*
     Match(Box<Expr<N>>, VecDeque<(Pattern<N>, Expr<N>)>, Span),
 
-    // Errors
+    // Errors. The `Span` contains the range of the *entire expression*
     Error(Box<Expr<N>>, Span),
 }
 
 pub type ArgList<A, N> = VecDeque<(A, Type<N>)>;
+
+// Computes the *true* range of a given expression.
+pub fn range<N> (e: Expr<N>) -> Span {
+    use Expr::*;
+    match e {
+        Variable(_, s) => s,
+        IntLiteral(_, s) => s,
+        BoolLiteral(_, s) => s,
+        StringLiteral(_, s) => s,
+        UnitLiteral(s) => s,
+        Plus(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Minus(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Times(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Div(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Mod(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        LessThan(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        LessEquals(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        And(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Or(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Equals(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Concat(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Not(ep, s) => join(s, range(*ep)),
+        Neg(ep, s) => join(s, range(*ep)),
+        Call(_, _, s) => s,
+        Sequence(lhs, rhs) => join(range(*lhs), range(*rhs)),
+        Let(_, _, _, _, s) => s,
+        Ite(_, _, _, s) => s,
+        Match(scrut, _, s) => join(range(*scrut), s),
+        Error(_, s) => s
+    }
+}
 
 // Pattern structure
 
